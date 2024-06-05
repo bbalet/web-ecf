2b63ebb6a9c22a6ccab511cf6c1bd19478d84ecc
Benjamin BALET <benjamin.balet@gmail.com>
2024-06-05 15:59:56 +0200

    add entries

--- src/Controller/UserSpaceController.php
+++ src/Controller/UserSpaceController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\OrderTicketsRepository;
+use App\Repository\ReviewRepository;
+use App\Repository\MovieRepository;
 
 class UserSpaceController extends AbstractController
 {
@@ -29,4 +31,24 @@ class UserSpaceController extends AbstractController
             'orders' => $orders
         ]);
     }
+
+    #[Route('/userspace/movies/{id}/review', name: 'app_userspace_movie_review')]
+    public function review(int $movieId, MovieRepository $movieRepository, ReviewRepository $reviewRepository): Response
+    {
+        // User needs to be authenticated to access the personal page
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        /** @var User */
+        $user = $this->getUser();
+
+        // Get the movie to review and throw an error if it does not exist
+        $movie = $movieRepository->findOneById($movieId);
+
+        // TODO form
+        // TODO update entity with a status (NEW, VALIDATED, REJECTED)
+        // TODO the average of the ratings is calculated with VALIDATED reviews
+        // TODO update the datafixtures scripts with all generated review to VALIDATED status
+
+        return $this->render('userspace/review.html.twig');
+    }
+
 }
